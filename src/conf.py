59cd345c905c54efbf201643c256e528f4368548
Ondřej Čertík <ondrej@certik.us>
2018-08-13 17:35:22 -0600

    Disable math_number_all = True
    Until https://github.com/sphinx-doc/sphinx/issues/5298 is fixed.

--- src/conf.py
+++ src/conf.py
@@ -362,7 +362,7 @@ latex_documents = [
 imgmath_image_format = 'svg'
 imgmath_dvisvgm_args = ['--no-fonts', '--exact']
 
-math_number_all = True
+#math_number_all = True
 math_numfig = True
 numfig = True
 numfig_secnum_depth = 3
