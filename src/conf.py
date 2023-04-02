76a571ee05fdc3fc85d4d7cae77741bdc0749791
Ondřej Čertík <ondrej@certik.us>
2023-04-01 20:13:06 -0600

    This compiles with tectonic

--- src/conf.py
+++ src/conf.py
@@ -181,18 +181,13 @@ latex_preamble = r"""
 
 """
 
-fontpkg = r"""\usepackage{fontspec}
-\setmainfont{texgyrepagella}[
-  Extension = .otf,
-  UprightFont = *-regular,
-  BoldFont = *-bold,
-  ItalicFont = *-italic,
-  BoldItalicFont = *-bolditalic,
-]
+fontpkg = r"""
+\usepackage{lmodern}
 """
 
 imgmath_latex_preamble = latex_preamble
-latex_elements = {"preamble": latex_preamble, "fontpkg": fontpkg}
+latex_elements = {"preamble": latex_preamble, "fontpkg": fontpkg,
+        "fontenc": ""}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
